9556dda2b09ead73dbe89ca31224ae95f6cb5b47
alejo2246 <alejopadilla297@gmail.com>
2023-01-11 16:35:33 -0500

    agregar respuesta incorrecta

--- CONFIRM/code.js
+++ CONFIRM/code.js
@@ -2,3 +2,6 @@ const contenedor = document.querySelector(".respuesta");
 
 contenedor.classList.add("green");
 contenedor.innerHTML = "hola";
+
+contenedor.classList.add("red");
+contenedor.innerHTML = "error";
