f753187f737e76074d0d7cd32cc8709db0afb978
alejo2246 <alejopadilla297@gmail.com>
2023-01-11 16:44:26 -0500

    .

--- CONFIRM/code.js
+++ CONFIRM/code.js
@@ -2,6 +2,8 @@ const contenedor = document.querySelector(".respuesta");
 
 contenedor.classList.add("green");
 contenedor.innerHTML = "hola";
+if(true){
+  contenedor.classList.add("red");
+  contenedor.innerHTML = "error";
 
-contenedor.classList.add("red");
-contenedor.innerHTML = "error";
+}
